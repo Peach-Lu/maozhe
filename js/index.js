3241b2f21e85dcd502c3a763ad5ff865076a7cf7
Peach <2990221345@qq.com>
2022-12-04 22:13:53 +0800

    commit

---
+++ js/index.js
@@ -0,0 +1,18 @@
+// 监听视频播放的进度
+const video = document.querySelector('.myVideo') ?? ''
+const content = document.querySelector('.content') ?? ''
+content.style.display = 'none'
+video.addEventListener('ended', function () {
+    video.style.display = 'none'
+    content.style.display = 'block'
+    console.log('content', content)
+    console.log('结束')
+})
+const backBtn = document.querySelector('.goBack') ?? ''
+backBtn.addEventListener('click', function () {
+    window.scrollTo({
+        left: 0,
+        top: 0,
+        behavior: 'smooth'
+    })
+})
