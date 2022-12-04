cabf9d1c8b9df011f53a1a77ab661cccdd66175c
Peach <2990221345@qq.com>
2022-12-04 22:52:23 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,13 +1,14 @@
-// 监听视频播放的进度
-const video = document.querySelector('.myVideo') ?? ''
-const content = document.querySelector('.content') ?? ''
-content.style.display = 'none'
-video.addEventListener('ended', function () {
-    video.style.display = 'none'
-    content.style.display = 'block'
-    console.log('content', content)
-    console.log('结束')
-})
+// // 监听视频播放的进度
+// const video = document.querySelector('.myVideo') ?? ''
+// const content = document.querySelector('.content') ?? ''
+// content.style.display = 'none'
+// video.addEventListener('ended', function () {
+//     video.style.display = 'none'
+//     content.style.display = 'block'
+//     window.location.href = 'index.html'
+//     console.log('content', content)
+//     console.log('结束')
+// })
 const backBtn = document.querySelector('.goBack') ?? ''
 backBtn.addEventListener('click', function () {
     window.scrollTo({
